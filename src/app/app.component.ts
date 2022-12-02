4c13b5281bc0ed62369645718121458a1fc9a875
Joseph Borne <josephpbourne@gmail.com>
2022-12-02 12:55:04 -0800

    learning arrays in ts and angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
-  age = 32;
+  tasks = [
+    'Visiting Ann',
+    'Call Dad',
+    'Go to the gym',
+    'Wash the dishes',
+    'Shop for the party',
+  ];
 }
