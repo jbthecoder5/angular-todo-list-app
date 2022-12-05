abb64846b45d61037100f48207d526a7d8072b3c
Joseph Borne <josephpbourne@gmail.com>
2022-12-05 13:40:11 -0800

    used the ng generate component command and generated a task-list component sub directory and files for the app-task-list. moved all html, css, and typescript code from app.component to the task list component. Learned how to encapsulate and view components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,36 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
-  tasks: Task[] = [
-    new Task('Visiting Ann'),
-    new Task('Go to the gym'),
-    new Task('Call Dad'),
-    new Task('Wash the dishes'),
-    new Task('Shop for the party'),
-  ];
-
-  // adding a new array to the list
-  add(newTask: string) {
-    this.tasks.push(new Task(newTask));
-  }
-
-  remove(existingTask: Task) {
-    var userConfirmed = confirm(
-      `Are you sure that you want to remove the following task? \n "${existingTask.title}"`
-    );
-
-    if (userConfirmed) {
-      this.tasks = this.tasks.filter((task) => task != existingTask);
-    }
-  }
-}
-
-class Task {
-  constructor(public title: string) {}
-
-  toggleIsDone() {
-    this.isDone = !this.isDone;
-  }
-
-  public isDone = false;
 }
