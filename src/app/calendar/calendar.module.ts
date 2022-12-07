013c14db97ed8ea8bae3fab95a18582897e14d92
Joseph Borne <josephpbourne@outlook.com>
2022-12-07 12:04:26 -0800

    learned about router-outlet tag, and how to create route links in angular.

--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -4,6 +4,7 @@ import { CalendarComponent } from './calendar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [CalendarComponent],
@@ -12,6 +13,7 @@ import { MatNativeDateModule } from '@angular/material/core';
     BrowserAnimationsModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    RouterModule,
   ],
   exports: [CalendarComponent],
 })
